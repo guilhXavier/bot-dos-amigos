5f162155502f5d63977dd7efbbc36d06d230144b
guilhXavier <guilhermetamara5@gmail.com>
2019-06-16 14:15:59 -0300

    parece q a funcionalidade da musica ta 100%

--- bot/bot.js
+++ bot/bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('../auth/auth.json');
 const ytdl = require('ytdl-core');
-
+const queue = new Map();
 const { prefix, token } = auth;
 
 client.on('ready', () => {
@@ -31,7 +31,97 @@ client.on('message', async msg => {
 
 async function execute(msg, serverQueue) {
 	const args = msg.content.split(' ');
+
 	const voiceChannel = msg.member.voiceChannel;
+
+	if (!voiceChannel)
+		return msg.channel.send('Vc precisa estar no canal pra tocar musica');
+
+	const permissions = voiceChannel.permissionsFor(msg.client.user);
+
+	if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
+		return msg.channel.send('Eu preciso de permissao pra isso :(');
+	}
+
+	const songInfo = await ytdl.getInfo(args[1]);
+
+	const song = {
+		title: songInfo.title,
+		url: songInfo.video_url
+	};
+
+	if (!serverQueue) {
+		const queueConstruct = {
+			textChannel: msg.channel,
+			voiceChannel,
+			connection: null,
+			songs: [],
+			volume: 5,
+			playing: true
+		};
+
+		queue.set(msg.guild.id, queueConstruct);
+
+		queueConstruct.songs.push(song);
+
+		try {
+			var connection = await voiceChannel.join();
+
+			queueConstruct.connection = connection;
+
+			play(msg.guild, queueConstruct.songs[0]);
+		} catch (error) {
+			console.log(error);
+
+			queue.delete(msg.guild.id);
+
+			return msg.channel.send(err);
+		}
+	} else {
+		serverQueue.songs.push(song);
+		console.log(serverQueue.songs);
+		return msg.channel.send(`${song.title} foi adicionada a lista de espera`);
+	}
+}
+
+function skip(msg, serverQueue) {
+	if (!msg.member.voiceChannel)
+		return msg.channel.send(
+			'Vc tem q ta no canal pra estragar a festa dos outros'
+		);
+	if (!serverQueue) return msg.channel.send('N tem musica pra skipar');
+	serverQueue.connection.dispatcher.end();
+}
+
+function stop(msg, serverQueue) {
+	if (!msg.member.voiceChannel)
+		return msg.channel.send('Vc tem que ta no canal pra parar a musica');
+	serverQueue.songs = [];
+	serverQueue.connection.dispatcher.end();
+}
+
+function play(guild, song) {
+	const serverQueue = queue.get(guild.id);
+
+	if (!song) {
+		serverQueue.voiceChannel.leave();
+		queue.delete(guild.id);
+		return;
+	}
+
+	const dispatcher = serverQueue.connection
+		.playStream(ytdl(song.url))
+		.on('end', () => {
+			console.log('cabo a musica');
+
+			serverQueue.songs.shift();
+
+			play(guild, serverQueue.songs[0]);
+		})
+		.on('error', error => {
+			console.error(error);
+		});
+	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
 }
 
 client.login(token);
