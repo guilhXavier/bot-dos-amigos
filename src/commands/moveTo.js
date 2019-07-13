78a7950ed7dcc9d40b6c59db82400c3560fb39fc
guilhXavier <guilhermetamara5@gmail.com>
2019-07-13 16:01:05 -0300

    Feature de mover usuarios de canais de voz adicionada

---
+++ src/commands/moveTo.js
@@ -0,0 +1,41 @@
+// $moveTo @NickBola#2333 VariosCanaisMasSóUsamosEsse
+exports.run = async (msg) => {
+  const {
+    content, guild, mentions, member,
+  } = msg;
+
+  const { channels } = guild;
+  const { members } = mentions;
+  const args = content.split(' ');
+
+  if (members.size !== 0) {
+    args.splice(0, 2);
+    const alvo = members.first(1)[0];
+
+    const canal = channels.find(val => val.name === args[0]);
+
+    await alvo.setVoiceChannel(canal);
+
+    return msg.channel.send({
+      embed: {
+        color: 9699539,
+        title: 'Move To',
+        description: `${alvo} foi movido ao canal ${canal.name}`,
+      },
+    });
+  }
+
+  const alvo = member;
+
+  const canal = channels.find(val => val.name === args[1]);
+
+  await alvo.setVoiceChannel(canal);
+
+  return msg.channel.send({
+    embed: {
+      color: 9699539,
+      title: 'Move To',
+      description: `${alvo} foi movido ao canal ${canal.name}`,
+    },
+  });
+};
