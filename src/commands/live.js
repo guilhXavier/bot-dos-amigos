b96f27b51ad35ae1cef00cea3448450ae0a7f5b7
guilhXavier <guilhermetamara5@gmail.com>
2019-07-23 18:52:41 -0300

    Funcionalidade de acompanhamento de partidas da HLTV ao vivo adicionada

--- src/commands/live.js
+++ src/commands/live.js
@@ -1,25 +1,39 @@
 const { HLTV } = require('hltv');
 
 // $live { id }
-exports.run = (msg) => {
+exports.run = async (msg) => {
   const args = msg.content.split(' ');
   const id = args[1];
 
+  const { team1, team2 } = await HLTV.getMatch({ id });
+
   HLTV.connectToScorebot({
     id,
-    onScoreboardUpdate: (data) => {
+    onConnect: () => {
       msg.channel.send({
         embed: {
           color: 9699539,
-          title: `Match: ${data.terroristTeamName} vs ${data.ctTeamName}`,
-          fields: [
-            {
-              name: 'Score',
-              value: `${data.ctTeamScore}:${data.tTeamScore}`,
-            },
-          ],
+          title: 'ScoreBot: Connection established',
         },
       });
+    },
+    onLogUpdate: (data) => {
+      if (Object.keys(data.log[0])[0] === 'RoundEnd') {
+        msg.channel.send({
+          embed: {
+            color: 9699539,
+            title: `Match: ${team1.name} vs ${team2.name} | id: ${id}`,
+            fields: [
+              {
+                name: 'Score',
+                value: `(CT)${data.log[0].RoundEnd.counterTerroristScore}:${
+                  data.log[0].RoundEnd.terroristScore
+                }(TR)`,
+              },
+            ],
+          },
+        });
+      }
     },
   });
 };
